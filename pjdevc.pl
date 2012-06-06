36bbde2eb05173189c557a483ef39c43cba011c4
Will Mitchell <will.mitchell@noblis.org>
2012-06-06 08:30:56 -0400

    update Ant

--- pjdevc.pl
+++ pjdevc.pl
@@ -24,8 +24,8 @@ groovy|GROOVY_HOME|http://dist.groovy.codehaus.org/distributions/groovy-binary-1
 grails|GRAILS_HOME|http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.0.0.zip|2.0.0|1
 griffon|GRIFFON_HOME|http://dist.codehaus.org/griffon/griffon/0.9.x/griffon-0.9.5-bin.zip|0.9.5|1
 ivy|IVY_HOME|http://mirror.cc.columbia.edu/pub/software/apache/ant/ivy/2.2.0/apache-ivy-2.2.0-bin.zip|2.2.0|1
-ant|ANT_HOME|http://www.eng.lsu.edu/mirrors/apache//ant/binaries/apache-ant-1.8.2-bin.zip|1.8.2|1
-maven|MAVEN_HOME|http://www.eng.lsu.edu/mirrors/apache//maven/binaries/apache-maven-3.0.3-bin.zip|3.0.3|1
+ant|ANT_HOME|http://www.eng.lsu.edu/mirrors/apache//ant/binaries/apache-ant-1.8.4-bin.zip|1.8.4|1
+maven|MAVEN_HOME|http://www.eng.lsu.edu/mirrors/apache//maven/binaries/apache-maven-3.0.4-bin.zip|3.0.4|1
 END
 
 my @lines = split /\n/, $db;
