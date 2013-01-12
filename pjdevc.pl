8959e8bf11fc62c08797e925eaba356636a742c7
Will Mitchell <will.mitchell@gmail.com>
2013-01-12 12:33:29 -0500

    modernize

--- pjdevc.pl
+++ pjdevc.pl
@@ -19,9 +19,9 @@ use Cwd;
 # Hardly a database, but these are the files, names, urls, versions, etc for the things we want to manage.
 #
 $db = <<END;
-gradle|GRADLE_HOME|http://services.gradle.org/distributions/gradle-1.2-bin.zip|1.2|1
+gradle|GRADLE_HOME|http://services.gradle.org/distributions/gradle-1.3-bin.zip|1.3|1
 groovy|GROOVY_HOME|http://dist.groovy.codehaus.org/distributions/groovy-binary-1.8.3.zip|1.8.3|1
-grails|GRAILS_HOME|http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.0.0.zip|2.0.0|1
+grails|GRAILS_HOME|http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.2.0.zip|2.2.0|1
 griffon|GRIFFON_HOME|http://dist.codehaus.org/griffon/griffon/0.9.x/griffon-0.9.5-bin.zip|0.9.5|1
 ivy|IVY_HOME|http://mirror.cc.columbia.edu/pub/software/apache/ant/ivy/2.2.0/apache-ivy-2.2.0-bin.zip|2.2.0|1
 ant|ANT_HOME|http://apache.mirrors.tds.net//ant/binaries/apache-ant-1.8.4-bin.zip|1.8.4|1
